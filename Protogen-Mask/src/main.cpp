4c9728bc61c9174cbcac38c2ce1512640e40ebcc
CrunchyWaterIsNotIce <lviray2006@gmail.com>
2026-03-07 14:06:58 -0600

    Constructed final* prototype for mask (w/out light earholes), and stressed test mask with a basic rainbow hue.

--- Protogen-Mask/src/main.cpp
+++ Protogen-Mask/src/main.cpp
@@ -2,33 +2,48 @@
 #include <FastLED.h>
 
 #define LED_PIN     25
-#define NUM_LEDS    256   
+#define NUM_LEDS    688 
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
 
-#define BRIGHTNESS  20 
+// set brightness to max because FastLED's power management will override it to 3A
+#define BRIGHTNESS  255 
+#define ONBOARD_LED 2 
 
 CRGB leds[NUM_LEDS];
 
+// variable to smoothly animate the rainbow
+uint8_t currentHue = 0; 
+
 void setup() {
   Serial.begin(115200);
-  Serial.println("Booting up...");
+  pinMode(ONBOARD_LED, OUTPUT);
 
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-  FastLED.setBrightness(BRIGHTNESS);
   
-  fill_solid(leds, NUM_LEDS, CRGB::Red);
-  FastLED.show();
-  delay(500);
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-  FastLED.show();
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 3000); 
+  FastLED.setBrightness(BRIGHTNESS);
+
+  FastLED.clear(true);
+
+  for(int i = 10; i > 0; i--) {
+    Serial.print(i);
+    Serial.println("...");
+    digitalWrite(ONBOARD_LED, HIGH); 
+    delay(500);                      
+    digitalWrite(ONBOARD_LED, LOW);  
+    delay(500);                      
+  }
+  
+  digitalWrite(ONBOARD_LED, HIGH); // LED ON to indicate system is running
 }
 
 void loop() {
-  for(int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Green;
-    FastLED.show();
-    delay(40);
-    leds[i] = CRGB::Black; // turns the led after off
+  fill_rainbow(leds, NUM_LEDS, currentHue, 7);
+  
+  FastLED.show();
+  
+  EVERY_N_MILLISECONDS(20) {
+    currentHue++; 
   }
 }
