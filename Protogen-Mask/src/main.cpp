1174bf94f311404c5060dd8921b44b09d3f5f40b
Lorenzo Viray <lviray2006@gmail.com>
2026-03-08 16:21:17 -0500

    Implemented basic pixel (.png) to matrix mapping and found specifc template for mouth, nose, and eyes.

--- Protogen-Mask/src/main.cpp
+++ Protogen-Mask/src/main.cpp
@@ -2,48 +2,141 @@
 #include <FastLED.h>
 
 #define LED_PIN     25
-#define NUM_LEDS    688 
+#define NUM_LEDS    432
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
-
-// set brightness to max because FastLED's power management will override it to 3A
-#define BRIGHTNESS  255 
+#define BRIGHTNESS  240 
 #define ONBOARD_LED 2 
 
 CRGB leds[NUM_LEDS];
 
-// variable to smoothly animate the rainbow
-uint8_t currentHue = 0; 
+// Mouth (right to left; top to bottom), Nose (left to right; top to bottom), Right Eye(right to left; bottom to top), Left Eye(right to left; top to bottom)
+
+const CRGB mouthSprite[] = {
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),  
+  CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(63, 63, 116), CRGB(63, 63, 116), CRGB(63, 63, 116), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(63, 63, 116), CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(0, 0, 0), CRGB(63, 63, 116),
+  CRGB(63, 63, 116), CRGB(63, 63, 116), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(48, 96, 130), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(48, 96, 130), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(0, 0, 0), CRGB(0, 0, 0),  
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(48, 96, 130), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), 
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(91, 110, 225), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(91, 110, 225), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(91, 110, 225), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(91, 110, 225), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(0, 0, 0),
+  CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(99, 155, 255), CRGB(99, 155, 255),
+  CRGB(0, 0, 0), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(91, 110, 225), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(91, 110, 225), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(91, 110, 225), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(91, 110, 225), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), 
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(48, 96, 130), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),  
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(48, 96, 130), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(48, 96, 130), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(63, 63, 116),
+  CRGB(63, 63, 116), CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(63, 63, 116), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(63, 63, 116), CRGB(63, 63, 116), CRGB(63, 63, 116), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),  
+};
+
+const CRGB noseSprite[] = {
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(48, 96, 130), CRGB(91, 110, 225), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(99, 155, 255), CRGB(203, 219, 252), CRGB(91, 110, 225),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(99, 155, 255),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(99, 155, 255), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(91, 110, 225), CRGB(203, 219, 252), CRGB(99, 155, 255), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(48, 96, 130),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0),
+};
+
+const CRGB eyeRightSprite[] = {
+  CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(63, 63, 116), CRGB(48, 96, 130), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(48, 96, 130), CRGB(48, 96, 130), CRGB(91, 110, 225), CRGB(99, 155, 255), CRGB(91, 110, 225), CRGB(91, 110, 225),
+  CRGB(91, 110, 225), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(91, 110, 225), CRGB(91, 110, 225), CRGB(203, 219, 252), CRGB(48, 96, 130), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(48, 96, 130), CRGB(91, 110, 225), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(91, 110, 225),
+  CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(203, 219, 252), CRGB(203, 219, 252), CRGB(99, 155, 255), CRGB(91, 110, 225), CRGB(0, 0, 0), CRGB(0, 0, 0),
+  CRGB(63, 63, 116), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(203, 219, 252), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(0, 0, 0),      
+  CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(0, 0, 0),       
+  CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(99, 155, 255),
+};
+
+const CRGB eyeLeftSprite[] = {
+  CRGB(99, 155, 255), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(91, 110, 225), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(99, 155, 255),       
+  CRGB(0, 0, 0), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(203, 219, 252), CRGB(91, 110, 225), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(63, 63, 116),      
+  CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(91, 110, 225), CRGB(99, 155, 255), CRGB(203, 219, 252), CRGB(203, 219, 252), CRGB(99, 155, 255), CRGB(99, 155, 255),
+  CRGB(91, 110, 225), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(91, 110, 225), CRGB(48, 96, 130), CRGB(48, 96, 130), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(203, 219, 252), CRGB(91, 110, 225), CRGB(91, 110, 225), CRGB(99, 155, 255), CRGB(99, 155, 255), CRGB(91, 110, 225),
+  CRGB(91, 110, 225), CRGB(91, 110, 225), CRGB(99, 155, 255), CRGB(91, 110, 225), CRGB(48, 96, 130), CRGB(48, 96, 130), CRGB(48, 96, 130), CRGB(0, 0, 0),
+  CRGB(0, 0, 0), CRGB(63, 63, 116), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(48, 96, 130), CRGB(63, 63, 116), CRGB(63, 63, 116), CRGB(0, 0, 0),
+};
 
 void setup() {
   Serial.begin(115200);
   pinMode(ONBOARD_LED, OUTPUT);
 
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-  
   FastLED.setMaxPowerInVoltsAndMilliamps(5, 3000); 
   FastLED.setBrightness(BRIGHTNESS);
-
   FastLED.clear(true);
 
-  for(int i = 10; i > 0; i--) {
-    Serial.print(i);
-    Serial.println("...");
-    digitalWrite(ONBOARD_LED, HIGH); 
-    delay(500);                      
-    digitalWrite(ONBOARD_LED, LOW);  
-    delay(500);                      
+  digitalWrite(ONBOARD_LED, HIGH);
+
+  // Mouth (starts at LED 0)
+  int mouthSize = sizeof(mouthSprite) / sizeof(mouthSprite[0]);
+  for (int i = 0; i < mouthSize; i++) {
+    leds[i] = mouthSprite[i];
   }
   
-  digitalWrite(ONBOARD_LED, HIGH); // LED ON to indicate system is running
+  // Nose (starts after mouth)
+  int noseSize = sizeof(noseSprite) / sizeof(noseSprite[0]);
+  for (int i = 0; i < noseSize; i++) {
+    leds[mouthSize + i] = noseSprite[i];
+  }
+  
+  // Right Eye (starts after nose)
+  int eyeRightSize = sizeof(eyeRightSprite) / sizeof(eyeRightSprite[0]);
+  for (int i = 0; i < eyeRightSize; i++) {
+    leds[mouthSize + noseSize + i] = eyeRightSprite[i];
+  }
+  
+  // Left Eye (starts after right eye)
+  int eyeLeftSize = sizeof(eyeLeftSprite) / sizeof(eyeLeftSprite[0]);
+  for (int i = 0; i < eyeLeftSize; i++) {
+    leds[mouthSize + noseSize + eyeRightSize + i] = eyeLeftSprite[i];
+  }
+
+  FastLED.show();
+  
+  Serial.print("Mouth: 0-");
+  Serial.println(mouthSize - 1);
+  Serial.print("Nose: ");
+  Serial.print(mouthSize);
+  Serial.print("-");
+  Serial.println(mouthSize + noseSize - 1);
+  Serial.print("Right Eye: ");
+  Serial.print(mouthSize + noseSize);
+  Serial.print("-");
+  Serial.println(mouthSize + noseSize + eyeRightSize - 1);
+  Serial.print("Left Eye: ");
+  Serial.print(mouthSize + noseSize + eyeRightSize);
+  Serial.print("-");
+  Serial.println(mouthSize + noseSize + eyeRightSize + eyeLeftSize - 1);
 }
 
 void loop() {
-  fill_rainbow(leds, NUM_LEDS, currentHue, 7);
-  
-  FastLED.show();
-  
-  EVERY_N_MILLISECONDS(20) {
-    currentHue++; 
-  }
+
 }
---
+++ Protogen-Mask/src/spritesheet_convert.py
@@ -0,0 +1,97 @@
+from PIL import Image
+
+## mouth
+mouth = Image.open('./assets/test_mouth_8x32.png')
+mouth = mouth.convert('RGB')
+mouth_map = mouth.load()
+
+m_width, m_height = mouth.size
+print('=' * 5, 'Mouth', '=' * 5)
+print(f"Image size: {m_width}x{m_height}")
+print(f"Total pixels: {m_width * m_height}")
+
+print("\nconst CRGB mouthSprite[] = {")
+for x in range(m_width - 1, -1, -1): # right to left
+    print("  ", end="")
+    if x % 2 == 0:
+        for y in range(m_height): # top to bottom
+            r, g, b = mouth_map[x, y]
+            print(f"CRGB({r}, {g}, {b}), ", end="")
+    else:
+        for y in range(m_height - 1, -1, -1): # bottom to top
+            r, g, b = mouth_map[x, y]
+            print(f"CRGB({r}, {g}, {b}), ", end="")
+    print()
+print("};")
+
+## nose
+nose = Image.open('./assets/test_nose_4x12.png')
+nose = nose.convert('RGB')
+nose_map = nose.load()
+
+n_width, n_height = nose.size
+print('=' * 5, 'Nose', '=' * 5)
+print(f"Image size: {n_width}x{n_height}")
+print(f"Total pixels: {n_width * n_height}")
+
+
+print("\nconst CRGB noseSprite[] = {")
+for x in range(n_width): # left to right
+    print("  ", end="")
+    if x % 2 == 0:
+        for y in range(n_height): # top to bottom
+            r, g, b = nose_map[x, y]
+            print(f"CRGB({r}, {g}, {b}), ", end="")
+    else:
+        for y in range(n_height - 1, -1, -1):
+            r, g, b = nose_map[x, y]
+            print(f"CRGB({r}, {g}, {b}), ", end="")
+    print()
+print("};")
+
+## eyes
+eyes = Image.open('./assets/test_eyes_8x8.png')
+eyes = eyes.convert('RGB')
+eyes_map = eyes.load()
+
+e_width, e_height = eyes.size
+print('=' * 5, 'Eyes', '=' * 5)
+print(f"Full image size: {e_width}x{e_height}")
+
+# Right Eye (last 8 columns from YOUR perspective, x=9 to x=16, skipping x=8 gap)
+print('=' * 5, 'Right Eye', '=' * 5)
+print(f"Image size: 8x{e_height}")
+print(f"Total pixels: {8 * e_height}")
+
+print("\nconst CRGB eyeRightSprite[] = {")
+for x in range(16, 8, -1):  # going right to left
+    print("  ", end="")
+    if x % 2 == 0:
+        for y in range(e_height - 1, -1, -1): # bottom to top
+            r, g, b = eyes_map[x, y]
+            print(f"CRGB({r}, {g}, {b}), ", end="")
+    else:
+        for y in range(e_height): # top to bottom
+            r, g, b = eyes_map[x, y]
+            print(f"CRGB({r}, {g}, {b}), ", end="")
+    print()
+print("};")
+
+# Left Eye (first 8 columns from YOUR perspective, x=0 to x=7)
+print('=' * 5, 'Left Eye', '=' * 5)
+print(f"Image size: 8x{e_height}")
+print(f"Total pixels: {8 * e_height}")
+
+print("\nconst CRGB eyeLeftSprite[] = {")
+for x in range(7, -1, -1):  # right to left
+    print("  ", end="")
+    if x % 2 == 0:
+        for y in range(e_height): # top to bottom
+            r, g, b = eyes_map[x, y]
+            print(f"CRGB({r}, {g}, {b}), ", end="")
+    else:
+        for y in range(e_height - 1, -1, -1): # bottom to top
+            r, g, b = eyes_map[x, y]
+            print(f"CRGB({r}, {g}, {b}), ", end="")
+    print()
+print("};")
